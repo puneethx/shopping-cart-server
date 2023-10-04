f97e24744aaadc29aebd8311fb5b440832638a10
Rion Dsilva <118284932+RionDsilvaCS@users.noreply.github.com>
2023-10-04 09:04:19 +0530

    bug fix

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"fmt"
 	"net/http"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+	// "github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/puneethx/shopping-cart-server/storage"
 	"database/sql"
 	// "github.com/joho/godotenv"
@@ -148,12 +148,12 @@ func main() {
 	}
 
 	app := fiber.New()
-	app.Use(cors.New(cors.Config{
-        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-        AllowOrigins:     "*",
-        AllowCredentials: true,
-        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-    }))
+	// app.Use(cors.New(cors.Config{
+    //     AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+    //     AllowOrigins:     "*",
+    //     AllowCredentials: true,
+    //     AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+    // }))
 
 	r.SetUpRoutes(app)
 
